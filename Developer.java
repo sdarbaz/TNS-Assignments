e0f48e655ddcc823926022e65502454a4f7d81bd
sdarbaz <162727840+sdarbaz@users.noreply.github.com>
2025-05-23 00:53:53 +0530

    Add files via upload

---
+++ Developer.java
@@ -0,0 +1,27 @@
+package com.syed.assignment.employees;
+
+/**
+ * Represents a Developer, extending Employee.
+ */
+public class Developer extends Employee {
+    private String programmingLanguage;
+
+    public Developer(String name, int employeeId, double salary, String programmingLanguage) {
+        super(name, employeeId, salary);
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    public String getProgrammingLanguage() {
+        return programmingLanguage;
+    }
+
+    public void setProgrammingLanguage(String programmingLanguage) {
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Programming Language: " + programmingLanguage);
+    }
+}
